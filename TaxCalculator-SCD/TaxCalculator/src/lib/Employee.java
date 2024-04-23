00772364bc103aa5da7edf27d7acdb5eb41219be
zynexe <zuhayrraffa@gmail.com>
2024-04-23 19:34:31 +0700

    Update Employee.java

--- TaxCalculator-SCD/TaxCalculator/src/lib/Employee.java
+++ TaxCalculator-SCD/TaxCalculator/src/lib/Employee.java
@@ -47,29 +47,21 @@ public class Employee {
 		childIdNumbers = new LinkedList<String>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
-	}
+	// Menggunakan enum untuk grade pegawai
+public enum Grade {
+    GRADE_1,
+    GRADE_2,
+    GRADE_3
+}
+
+public void setMonthlySalary(Grade grade) {
+    int[] gradeSalaries = {3000000, 5000000, 7000000};
+    int salary = gradeSalaries[grade.ordinal()];
+    if (isForeigner) {
+        salary *= 1.5;
+    }
+    monthlySalary = salary;
+}
 	
 	public void setAnnualDeductible(int deductible) {	
 		this.annualDeductible = deductible;
