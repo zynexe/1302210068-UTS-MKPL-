0c9eb88e8d4c5093d99e3bab03fd42c83f9a6779
zynexe <zuhayrraffa@gmail.com>
2024-04-23 20:12:11 +0700

    simplify tax calculation
    menyederhanakan penghitungan pajak penghasilan.

--- TaxCalculator-SCD/TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator-SCD/TaxCalculator/src/lib/TaxFunction.java
@@ -3,42 +3,28 @@ package lib;
 public class TaxFunction {
 
 	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
 		int tax = 0;
-		
+		int taxFreeIncome = calculateTaxFreeIncome(isMarried, numberOfChildren);
 		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
+			System.err.println("Lebih dari 12 bulan bekerja dalam setahun");
 		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+		tax = calculateTaxAmount(monthlySalary, otherMonthlyIncome, numberOfMonthWorking, deductible, taxFreeIncome);
+		return (tax < 0) ? 0 : tax;
 	}
+	
+	private static int calculateTaxAmount(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, int taxFreeIncome) {
+		return (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - taxFreeIncome));
+	}
+	
+	private static int calculateTaxFreeIncome(boolean isMarried, int numberOfChildren) {
+		int taxFreeIncome = 54000000;
+		if (isMarried) {
+			taxFreeIncome += 4500000;
+		}
+		taxFreeIncome += Math.min(numberOfChildren, 3) * 1500000;
+		return taxFreeIncome;
+	}
+	
 	
 }
